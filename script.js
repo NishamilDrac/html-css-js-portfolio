5d40e8315d70613a6eaac2454d100d533892bbd8
Nishamil_P <nishamilp@gmail.com>
2024-02-01 15:22:20 +0530

    still going on

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+    // window.location.href = "https://wa.me/+917510365239";
   }
   
